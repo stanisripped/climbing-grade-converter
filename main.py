e4e31abbcba370e18ce078fcb04f31a119996a79
Stan <s_yu26@u.pacific.edu>
2022-12-26 11:04:11 -0800

    enter Font grade as input and retrieve V-grade

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ def get_grade():
     return input("Enter a climbing grade:")
 
 def main():
-    '''
-    Create a dictionary with V-scale as keys and Font-scale as values
-    '''
+    # Create a dictionary with V-scale as keys and Font-scale as values
     grade_list = {}
-    grade = get_grade()
-    print(grade)
+    with open('conversions.txt') as f:
+        lines = f.readlines()
+        for line in lines:
+            grade = line.split()
+            # print(grade[1] + " " + grade[0])
+            grade_list[grade[1]] = grade[0]
+            print(grade[1])
+
+    input_grade = get_grade()
+    print(grade_list[input_grade])
 
 if __name__ == "__main__":
     main()
